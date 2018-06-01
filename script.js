495b4aa3ef357fcb9bfc08df6781d4deb1ac9d97
dev1203 <devsingh120397@gmail.com>
2018-05-31 20:40:49 -0700

    New PR

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ $("#down").on("click",function(){
 	$("#down").fadeOut();	
 	$("#landing-page").slideUp(1000,function(){
 		$(".info").fadeIn(500,);
+			$("#up").fadeIn();
+
 	});
 
 });
 
+$("#up").on("click",function(){
+	$("#up").fadeOut();
+	$("#landing-page").slideDown(1000,function(){
+		$(".info").fadeOut(500,);
+		$("#down").fadeIn();	
 
+	});
 
-// $("#rew").flip();
+});
