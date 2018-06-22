4931f39706689cb409b7551cd755603382562aa3
dev1203 <devsingh120397@gmail.com>
2018-06-21 22:29:08 -0700

    Refactored Code

--- script.js
+++ script.js
@@ -1,27 +1,182 @@
+function initMap() {
+    var uluru = {
+        lat: 49.2327,
+        lng: -123.3407
+    };
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 11,
+            scrollwheel: false,
+            mapTypeControl: false,
+            zoomControl: false,
+            scaleControl: false,
+            streetViewControl: false,
+            fullscreenControl: false,
+            center: uluru,
+            styles: [{
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#242f3e'
+                    }]
+                },
+                {
+                    elementType: 'labels.text.stroke',
+                    stylers: [{
+                        color: '#242f3e'
+                    }]
+                },
+                {
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#746855'
+                    }]
+                },
+                {
+                    featureType: 'administrative.locality',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#263c3f'
+                    }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#6b9a76'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#38414e'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry.stroke',
+                    stylers: [{
+                        color: '#212a37'
+                    }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#9ca5b3'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#746855'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{
+                        color: '#1f2835'
+                    }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#f3d19c'
+                    }]
+                },
+                {
+                    featureType: 'transit',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#2f3948'
+                    }]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#d59563'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{
+                        color: '#17263c'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{
+                        color: '#515c6d'
+                    }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{
+                        color: '#17263c'
+                    }]
+                }
+            ]
 
-$("#down").on("click",function(){
-	$("#down").fadeOut();	
-	$("#landing-page").slideUp(1000,function(){
-		$(".info").fadeIn(500,);
-			$("#up").fadeIn();
-				$(".box").fadeIn(1000);
-	});
 
-});
 
-$("#up").on("click",function(){
-	$("#up").fadeOut();
-	$("#landing-page").slideDown(1000,function(){
-		$(".info").fadeOut(500,);
-		$("#down").fadeIn();	
+        });
+    // The marker, positioned at Uluru
+    // var marker = new google.maps.Marker({position: uluru, map: map});
 
-	});
-
-});
-
-  $("#right").click(function () {
-		$(".info").fadeOut(500,function(){
-				$("#bottom").fadeIn(500);
-
-		});
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        position: {
+            lat: 49.2827,
+            lng: -123.1207
+        },
+        title: 'I am in Vancouver right now'
     });
+}
+$("#down").on("click", function() {
+    $("#down").fadeOut();
+    $("#landing-page").slideUp(1000, function() {
+        $(".info").fadeIn(500, );
+        $("#up").fadeIn();
+        $(".box").fadeIn(1000);
+    });
+
+});
+
+$("#up").on("click", function() {
+    $("#up").fadeOut();
+    $("#landing-page").slideDown(1000, function() {
+        $(".info").fadeOut(500, );
+        $("#down").fadeIn();
+
+    });
+
+});
+
+$("#right").click(function() {
+    $(".info").fadeOut(500, function() {
+        $("#bottom").fadeIn(500);
+
+    });
+});
